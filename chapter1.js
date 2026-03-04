da2f4b3247c9681c1e560dc0380001bd18d17c51
manaheeljamil <manaheeljamil20@gmail.com>
2026-03-04 10:26:51 +0500

    chapter 2

--- chapter1.js
+++ chapter1.js
@@ -1,3 +1,12 @@
-function text(){
+function text() {
     document.getElementById("product").className = "box2"
+}
+function getData(event) {
+    event.preventDefault()
+    var email = document.getElementById("exampleInputEmail1").value
+    var password = document.getElementById("exampleInputPassword1").value
+    console.log(email, password)
+}
+function change(){
+    document.getElementById("image").src="./img2.jp"
 }
